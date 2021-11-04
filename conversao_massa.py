a41c8f19aa5ac2d35225e5694262626e9a02f5e2
Eduardo Amarante <eamarante80@gmail.com>
2021-11-03 23:29:30 -0300

    some bugs resolved

--- conversao_massa.py
+++ conversao_massa.py
@@ -1,11 +1,8 @@
 from pathlib import Path
-from PIL import Image, ImageChops, ImageFilter
-from matplotlib import image, pyplot as plt
-from datetime import datetime
+from PIL import Image
 
 b = Image.open("./fundo_blue.png")
 r = Image.open("./fundo_red.png")
-#t = Image.open("./thumb.jpg")
 
 path = Path("C:/Users/Eduardo/Desktop/script_imagens")
 
@@ -14,20 +11,21 @@ for filename  in path.glob('*'):
     if ("jpg" or "JPG") in str(filename):
         
     
-        t = Image.open(filename)
+        thumb_jpg = Image.open(filename)
 
-        t = t.resize((1080,608),Image.ADAPTIVE)
+        thumb_jpg = thumb_jpg.resize((1080,608),Image.ADAPTIVE)
 
-        cor = "blue"
+        color = input("Digite blue ou red: ")
+        try:
+            if color == "red":
+                background = r.copy()
+            else:
+                background = b.copy()
+        except:
+            print('erro')
 
-        if cor == "red":
-            back = r.copy()
-        else:
-            back = b.copy()
-
-
-        back.paste(t,(0, 236))
+        background.paste(thumb_jpg,(0, 236))
 
         titulo = ((str(filename).split("\\"))[5]).replace(".jpg",".png")
         
-        back.save(str(path)+"/editados/{}".format(titulo))
+        background.save(str(path)+"/editados/{}".format(titulo))
