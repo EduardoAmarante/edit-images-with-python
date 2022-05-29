45059ca1a590cdd6e92ceff921f0f211a01717cf
Eduardo Amarante <51537878+EduardoAmarante@users.noreply.github.com>
2022-05-28 22:28:55 -0300

    update
    script works in any path

--- conversao_massa.py
+++ conversao_massa.py
@@ -1,34 +1,46 @@
 from pathlib import Path
+from time import sleep
 from PIL import Image
+import os,sys
 
-b = Image.open("./fundo_blue.png")
-r = Image.open("./fundo_red.png")
-g = Image.open("./fundo_green.png")
-pink = Image.open("./fundo_pink.png")
 
-path = Path("C:/Users/Eduardo/Desktop/script_imagens")
+def getPath():
+    return Path(os.path.abspath(os.path.dirname(sys.argv[0])))
+
+pdef = "C:/Users/Eduardo/Desktop/config/"
+b = Image.open(pdef + "fundo_blue.png")
+r = Image.open(pdef + "fundo_red.png")
+g = Image.open(pdef + "fundo_green.png")
+pink = Image.open(pdef + "fundo_pink.png")
+
+path = getPath()
 
 color = input("Digite blue/red/green/pink: ")
+print(path)
 
 for filename  in path.glob('*'):
+    try:
+        if ("jpg" or "JPG") in str(filename):
 
-    if ("jpg" or "JPG") in str(filename):
+            thumb_jpg = Image.open(filename)
 
-        thumb_jpg = Image.open(filename)
+            thumb_jpg = thumb_jpg.resize((1080,608),Image.ADAPTIVE)
 
-        thumb_jpg = thumb_jpg.resize((1080,608),Image.ADAPTIVE)
+            if color == "red":
+                background = r.copy()
+            elif color == "green":
+                background = g.copy()
+            elif color == "pink":
+                background = pink.copy()
+            elif color == "blue":
+                background = b.copy()
 
-        if color == "red":
-            background = r.copy()
-        elif color == "green":
-            background = g.copy()
-        elif color == "pink":
-            background = pink.copy()
-        else:
-            background = b.copy()
+            background.paste(thumb_jpg,(0, 236))
 
-        background.paste(thumb_jpg,(0, 236))
+            titulo = (str(filename)[str(filename).rfind('\\')+1:]).replace(".jpg",".png")
 
-        titulo = ((str(filename).split("\\"))[5]).replace(".jpg",".png")
-
-        background.save(str(path) + "/editados/{}" .format(titulo))
+            background.save(str(path) + "/" + titulo)
+            background.close()
+    except Exception as e:
+        s = str(e)
+        sleep(10)
